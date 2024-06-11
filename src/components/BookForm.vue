b57e43b42fbf201d89ac05fb8eaa0280f5ea4d1d
luizhf42 <luizhf42@gmail.com>
2024-06-11 18:19:30 -0300

    feat: improve form UI

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,17 +1,12 @@
 <template>
-	<div>
-		<form @submit.prevent="handleAddBook">
-			<label for="bookName"
-				>Title: <input id="bookName" v-model="bookTitle" type="text"
-			/></label>
-
-			<label for="bookAuthor"
-				>Author: <input id="bookAuthor" v-model="bookAuthor" type="text"
-			/></label>
-
-			<button>Add Book</button>
-		</form>
-	</div>
+	<form @submit.prevent="handleAddBook">
+		<h2>Add a book</h2>
+		<label><span>Title:</span> <input v-model="bookTitle" type="text" /></label>
+		<label
+			><span>Author:</span> <input v-model="bookAuthor" type="text"
+		/></label>
+		<button>Add Book</button>
+	</form>
 </template>
 
 <script setup lang="ts">
@@ -34,18 +29,31 @@ const handleAddBook = () => {
 <style scoped>
 form {
 	display: flex;
+	width: min(90%, 600px);
 	flex-direction: column;
-	align-items: flex-start;
+	align-items: center;
+	padding: 20px 10px;
 	margin-bottom: 20px;
+	background: #f9f9f9;
+	border-radius: 12px;
+	box-shadow: 0 0 4px 0px #0000004d;
 }
 
 label {
 	margin: 10px 0 5px;
-}
+	display: flex;
+	flex-direction: column;
+	width: 70%;
 
-input {
-	margin-bottom: 10px;
-	padding: 5px;
-	width: 300px;
+	span {
+		width: min-content;
+		font-size: 1.1rem;
+		cursor: text;
+	}
+
+	input {
+		margin-bottom: 10px;
+		padding: 5px;
+	}
 }
 </style>
