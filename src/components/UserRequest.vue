59688ffe225d9f40129b09eab0a17e8a378d7c19
luizhf42 <luizhf42@gmail.com>
2024-08-30 18:00:58 -0300

    feat: create user request message component

---
+++ src/components/UserRequest.vue
@@ -0,0 +1,20 @@
+<template>
+	<p>Suggest me books similar to:</p>
+	<ul>
+		<li v-for="(book, index) in books" :key="index">
+			<span>{{ book.title }}, by {{ book.author }}</span>
+		</li>
+	</ul>
+</template>
+
+<script setup lang="ts">
+import { useBooksStore } from "../store/books";
+
+const { books } = useBooksStore();
+</script>
+
+<style scoped lang="postcss">
+ul {
+  @apply list-disc list-inside pl-5;
+}
+</style>
